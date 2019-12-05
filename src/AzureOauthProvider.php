47c8ae2dd8d87e8db57994acdd8bd818b968607b
Jérémy Dillenbourg <jdi@bepark.eu>
2019-12-05 10:04:31 +0100

    clean

--- src/AzureOauthProvider.php
+++ src/AzureOauthProvider.php
@@ -68,7 +68,6 @@ class AzureOauthProvider extends AbstractProvider implements ProviderInterface
             'id'                => $user['id'],
             'name'              => $user['displayName'],
             'email'             => $user['mail'],
-
             'businessPhones'    => $user['businessPhones'],
             'displayName'       => $user['displayName'],
             'givenName'         => $user['givenName'],
